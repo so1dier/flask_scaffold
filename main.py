c4473bafb4775bc63140e89dcafbb2d4c84014d5
Daniel Golitsynski <don.aus@gmail.com>
2023-02-13 06:35:02 +0000

    Testing cicd

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     '''Return a friendly HTTP greeting.'''
     print('I am inside hello world')
-    return 'Hello World! CD, testing continuous delivery'
+    return 'Hello World! CD, testing continuous delivery. Something is not working'
 
 @app.route('/echo/<name>')
 def echo(name):
